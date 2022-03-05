f2d5871f18822fab3ce941102e1df693c3274824
Neil Penning <neil.penning@gmail.com>
2022-03-05 16:40:14 -0500

    better comments

--- Putnam/get_putnam_exam.sh
+++ Putnam/get_putnam_exam.sh
@@ -1,5 +1,5 @@
-#!bin/bash
-# Pull the Exams from the Archive
+#!/bin/bash
+# Pull the Exams from the Archive (ignore failure)
 curl -f "https://kskedlaya.org/putnam-archive/[1985-2022].pdf" -O --remote-name
 # Pull 2022 [sic] Exams from maa
 curl "https://www.maa.org/sites/default/files/pdf/AMC/Putnam 2021F session A problems.pdf" -o 2022a.pdf
--- Putnam/get_putnam_solutions.sh
+++ Putnam/get_putnam_solutions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Pull the Solutions from the Archive
+# Pull the Solutions from the Archive (ignore failure)
 curl -f "https://kskedlaya.org/putnam-archive/[1985-2021]s.pdf" -O --remote-name
 # Pull 2022 [sic] Solutions from maa
 curl "https://www.maa.org/sites/default/files/pdf/AMC/Putnam 2021 session A solutions.pdf" -o 2022a_s.pdf
