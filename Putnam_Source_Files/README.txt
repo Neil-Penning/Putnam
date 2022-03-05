6b1028732461e334ef76c7f9b125b39a430ef043
Neil Penning <neil.penning@gmail.com>
2022-03-05 16:49:47 -0500

    updated get all putnam exams and added instructions

---
+++ Putnam_Source_Files/README.txt
@@ -0,0 +1,3 @@
+Run
+    sh ./get_putnam_all.sh
+in this folder to download all the putnam exams and pdfs before you compile the LaTeX
--- Putnam_Source_Files/get_putnam_all.sh
+++ Putnam_Source_Files/get_putnam_all.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
 # get all putnam exams and solutions in tex and pdf format from 1985-2022
 # ignore failure (-f),
 # save to same file name as remote
 curl -f "https://kskedlaya.org/putnam-archive/[1985-2022]{,s}.{pdf,tex}" -O --remote-name
+curl "https://www.maa.org/sites/default/files/pdf/AMC/Putnam 2021F session A problems.pdf" -o 2022a.pdf
+curl "https://www.maa.org/sites/default/files/pdf/AMC/Putnam 2021F session B problems.pdf" -o 2022b.pdf
+curl "https://www.maa.org/sites/default/files/pdf/AMC/Putnam 2021 session A solutions.pdf" -o 2022a_s.pdf
+curl "https://www.maa.org/sites/default/files/pdf/AMC/Putnam 2021 session B solutions.pdf" -o 2022b_s.pdf
